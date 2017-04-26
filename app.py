a5a44b4f6ce142c08a94b944fd0958da7a6ac753
plark42 <marcrubin6@hotmail.com>
2017-04-26 15:43:16 -0700

    changing git username to plark42

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 from flask import Flask
 app = Flask(__name__)
 
-@app.route('/index.html')
 @app.route('/')
+@app.route('/index.html')
 def index():
     return app.send_static_file('index.html') 
 

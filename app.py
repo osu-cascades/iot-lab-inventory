a91a517ed24f900107b1568c1fb4a319ff93bb1f
plark42 <marcrubin6@gmail.com>
2017-04-26 09:47:23 -0700

    add static admin, student html; menu bar

--- app.py
+++ app.py
@@ -9,6 +9,22 @@ def hello():
 def css():
     return app.send_static_file('main.css')
     
+@app.route("/main_admin.html")
+def main_admin():
+    return app.send_static_file('main_admin.html')
+
+@app.route("/main_student.html")
+def main_student():
+    return app.send_static_file('main_student.html')
+
 if __name__ == "__main__":
     app.run()
+
+@app.after_request
+def add_header(r):
+    r.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    r.headers["Pragma"] = "no-cache"
+    r.headers["Expires"] = "0"
+    r.headers['Cache-Control'] = 'public, max-age=0'
+    return r
 

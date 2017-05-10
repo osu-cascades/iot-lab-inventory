72f21c62e0036e02601ef6122f21a79b045bb7dc
plark42 <marcrubin6@gmail.com>
2017-05-09 18:50:20 -0700

    added google authentication and basic login/logout

--- app.py
+++ app.py
@@ -1,17 +1,23 @@
-from flask import Flask, g, url_for, redirect, request, render_template, flash
-# from flask_googlelogin import GoogleLogin
-from werkzeug.local import LocalProxy
 import os
 import dataset
+import json
+from flask import Flask, g, url_for, redirect, request, render_template, flash, session
+from flask_login import UserMixin, login_required, login_user, logout_user, current_user
+from werkzeug.local import LocalProxy
+from flask_googlelogin import GoogleLogin
 
 
 app = Flask(__name__)
 app.debug = True
 app.secret_key = os.urandom(24)
-# googleLogin = GoogleLogin(app)
+app.config.update(
+    #SECRET_KEY='Miengous3Xie5meiyae6iu6mohsaiRae',
+    GOOGLE_LOGIN_CLIENT_ID='256102919201-qvjgcstktj5bimq3ffj8k5va85g0r137.apps.googleusercontent.com',
+    GOOGLE_LOGIN_CLIENT_SECRET='X0HqD1zykF3i4Hvde2QsvkdL',
+    GOOGLE_LOGIN_REDIRECT_URI='http://localhost:5000/oauth2callback')
+googlelogin = GoogleLogin(app)
 
 DATABASE_URL = 'sqlite:///data.sqlite'
-
 
 # Helpers
 
@@ -27,7 +33,6 @@ def teardown_db(exception):
     # NOOP. dataset API has no close() method.
     return
 
-
 db = LocalProxy(get_db)
 
 
@@ -36,8 +41,8 @@ db = LocalProxy(get_db)
 @app.route('/', methods=['GET'])
 def home():
     parts = db['parts'].all()
-    return render_template('home.html', parts=parts)
-
+    #return render_template('home.html', parts=parts)
+    return render_template('home.html', parts=parts, user=current_user)
 
 # Parts
 
@@ -102,19 +107,55 @@ def parts_delete(id):
     flash('Part ' + str(id) + ' has been deleted.')
     return redirect(url_for('parts_list'))
 
-# #Google Login auth stuffs
-# @app.route('/oauth2callback')
-# @googlelogin.oauth2callback
-# def create_or_update_user(token, userinfo, **params):
-#     user = User.filter_by(google_id=userinfo['id']).first()
-#     if user:
-#         user.name = userinfo['name']
-#         user.avatar = userinfo['picture']
-#     else:
-#         user = User(google_id=userinfo['id'],
-#                     name=userinfo['name'],
-#                     avatar=userinfo['picture'])
-#     db.session.add(user)
-#     db.session.flush()
-#     login_user(user)
-#     return redirect(url_for('index'))
+#logic for enabling Google Authentication
+
+users = {}
+
+class User(UserMixin):
+    def __init__(self, userinfo):
+        self.id = userinfo['id']
+        self.name = userinfo['name']
+        self.picture = userinfo.get('picture')
+
+
+@googlelogin.user_loader
+def get_user(userid):
+    return users.get(userid)
+
+
+@app.route('/login')
+def login():
+    #redirect user to log in with Google credentials
+    return redirect(googlelogin.login_url(approval_prompt='force',scopes=['https://www.googleapis.com/auth/userinfo.email']))
+
+@app.route('/profile')
+@login_required
+def profile():
+    return """
+        <p>Hello, %s</p>
+        <p><img src="%s" width="100" height="100"></p>
+        <p>Token: %r</p>
+        <p>Extra: %r</p>
+        <p><a href="/logout">Logout</a></p>
+        """ % (current_user.name, current_user.picture, session.get('token'),session.get('extra'))
+
+
+@app.route('/oauth2callback')
+@googlelogin.oauth2callback
+def login_oauth(token, userinfo, **params):
+    user = users[userinfo['id']] = User(userinfo)
+    login_user(user)
+    session['token'] = json.dumps(token)
+    session['extra'] = params.get('extra')
+    #return redirect(params.get('next', url_for('.profile')))
+    return redirect(params.get('next', url_for('.home')))
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    session.clear()
+    return redirect(url_for('home'))
+
+#start the app
+if __name__ == '__main__':
+    app.run()

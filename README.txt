2dbf6abc1cda4185298c310752d6cdbf42a8d0ec
plark42 <marcrubin6@gmail.com>
2017-05-17 10:01:55 -0700

    added Image, Document, and InventoryItem models; updated db relationships

--- README.txt
+++ README.txt
@@ -7,10 +7,16 @@ UNIX> python
 >>> db.create_all()
 >>> exit()
 
-TODO:
-- setup Flask-SQLAlchemy migration scripts
-- test, test, test login / logout w/ Google auth
+TODO: (prioritize on working software)
 - populate parts database
+- update view to show updated data
+- change google login to use oauth2 module
+
+- setup Flask-SQLAlchemy migration scripts
 - add User roles (e.g., admin vs. student)
 - add "cart" feature
 - add "checkout" cart
+
+per Yong:
+- clean up virtualenv wrapper (try UNIX> workon) instead of just virtualenv
+- look at github issue as using google login => use oauth
--- iot_app/models.py
+++ iot_app/models.py
@@ -7,9 +7,35 @@ class Part(db.Model):
     name = db.Column(db.String(64))
     description = db.Column(db.String(2048))
     category = db.Column(db.String(64))
+    sparkfun_id = db.Column(db.String(64))
+    images = db.relationship('Image', backref='part')
+    documents = db.relationship('Document', backref='part')
+    inventory_item = db.relationship('InventoryItem', back_populates='part', uselist=False)
+
+class Image(db.Model):
+    __tablename__ = 'images'
+    id = db.Column(db.Integer, primary_key=True)
+    filename = db.Column(db.String, default=None, nullable=True)
+    part_id = db.Column(db.Integer, db.ForeignKey('parts.id'))
+
+class Document(db.Model):
+    __tablename__ = 'documents'
+    id = db.Column(db.Integer, primary_key=True)
+    filename = db.Column(db.String, default=None, nullable=True)
+    part_id = db.Column(db.Integer, db.ForeignKey('parts.id'))
+
+class InventoryItem(db.Model):
+    __tablename__ = 'inventory_items'
+    id = db.Column(db.Integer, primary_key=True)
+    quantity = db.Column(db.Integer)
+    part_id = db.Column(db.Integer, db.ForeignKey('parts.id'))
+    part = db.relationship("Part", back_populates='inventory_item', uselist=False)
+
 
 class User(db.Model, UserMixin):
     __tablename__ = 'users'
+    #add id column (int) as primary key
+    #change id to google_id
     id = db.Column(db.String(256), primary_key=True, unique=True)
     name = db.Column(db.String(64))
     picture = db.Column(db.String(64))

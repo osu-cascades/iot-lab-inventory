1e6bf3b3530d37fb92a7b1c7d2727cc28bace38e
plark42 <marcrubin6@gmail.com>
2017-05-17 20:27:08 -0700

    added scrape_data.py and all_parts.csv

---
+++ scrape_data.py
@@ -0,0 +1,116 @@
+import csv
+import sys
+import requests
+from bs4 import BeautifulSoup
+import urllib2
+import os
+from iot_app.models import InventoryItem, Part, Image, Document
+import flask_sqlalchemy
+from iot_app import db
+
+#column number in csv file
+SKU = 0
+NUM = 1
+NAME = 4
+URL = 2
+
+f = open(sys.argv[1], 'r')
+reader = csv.reader(f)
+headers = reader.next()
+
+for row in reader:
+    part = Part()
+    name = row[NAME]
+    sku = row[SKU]
+    sku_num = sku.split('-')[1]
+    part.name = name
+    part.category = 'misc'
+    part.sparkfun_id = sku
+
+    num = row[NUM]
+    url = row[URL]
+
+    inventoryItem = InventoryItem()
+    inventoryItem.quantity = num
+    part.inventory_item = inventoryItem
+
+    print 'grabbing data for "' + name + '"'
+
+    #get the part's page
+    page = requests.get(url)
+    soup = BeautifulSoup(page.content, 'html.parser')
+
+    ## create a directory based on the part's name
+    dirname = 'part_resources/'
+    # try:
+    #     os.mkdir(dirname)
+    # except Exception as e:
+    #     pass
+
+    # #get part name (from page)
+    # #TODO: add name to db
+    # divs = soup.find_all('div', class_='product-title')
+    # for d in divs:
+    #     h1 = d.find('h1')
+    #     if h1 is not None:
+    #         print h1.text
+
+    #get part description
+    #TODO: add description to db
+    divs = soup.find_all('div', class_="description")
+    for d in divs:
+        p = d.find('p')
+        if p is not None and 'Description' in p.text:
+            part.description = p.text
+
+    #grab all jpg images, store in file system
+    #TODO: add image filename(s) to db
+    images = soup.find_all('img')
+    i = 1
+    for image in images:
+        src = image.get('src')
+        if '.jpg' in src and sku_num in src:
+            #store image in part_name_1.jpg, 2.jpg, ...
+            image_data = urllib2.urlopen(src).read()
+            filename = name.replace(' ', '_').replace('/','_') + '_' + str(i) + '.jpg'
+            with open(dirname + filename, "wb") as image_file:
+                image_file.write(image_data)
+            imageObj = Image(filename=filename)
+            imageObj.part = part
+            i += 1
+
+    #grab all pdf documentation, store in file system
+    links = soup.find_all('a')
+    for link in links:
+        href = link.get('href')
+        if href is not None \
+                and '.pdf' in href \
+                and 'http' in href \
+                and 'micrel' not in href\
+                and 'interlinkelectronics' not in href:
+            pdf_name = href.split('/')[-1]
+            pdf_name = pdf_name.replace('%','_')
+            try:
+                pdf_data = urllib2.urlopen(href).read()
+                filename = name.replace(' ', '_').replace('/','_') + '_' + pdf_name
+                with open(dirname + filename, 'wb') as pdf_file:
+                    pdf_file.write(pdf_data)
+
+                document = Document(filename=filename)
+                document.part = part
+            except Exception as e:
+                pass
+
+        if link.string == 'Hookup Guide':
+            if href is not None and 'learn' in href:
+                filename = name.replace(' ','_').replace('/','_') + '_hookup_guide.pdf'
+                cmd = "wkhtmltopdf '" + href + "' " + '"' + dirname + filename + '"'
+                document = Document(filename=filename)
+                document.part=part
+                os.system(cmd)
+
+    #put part into database
+    db.session.add(part)
+    db.session.commit()
+
+f.close()

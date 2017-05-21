008448008e66ccef4947428043cfa9525ec8aeda
Marc Rubin <marcrubin6@gmail.com>
2017-05-21 14:52:32 -0700

    added edit2.html and forms.py to use Bootstrap and WTF-Forms to render edit form

---
+++ iot_app/forms.py
@@ -0,0 +1,28 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, IntegerField, SelectField, TextAreaField
+from wtforms.validators import Required, NumberRange
+
+category_index = { 'actuator':0,
+                     'cable':1,
+                     'controller':2,
+                     'kit':3,
+                     'misc':4,
+                     'sensor':5,
+                     'tool':6,
+                     'wireless':7}
+
+category_choices = [ (0,'actuator'),
+                     (1,'cable'),
+                     (2,'controller'),
+                     (3,'kit'),
+                     (4,'misc'),
+                     (5,'sensor'),
+                     (6,'tool'),
+                     (7,'wireless')]
+
+class EditPartForm(FlaskForm):
+    name = StringField('Name', validators=[Required()])
+    description = TextAreaField('Description', validators=[Required()])
+    quantity = IntegerField('Quantity', validators=[NumberRange(min=0,max=1000,message='Quantity must be 0 or more!')])
+    category = SelectField('Category', coerce=int, choices=category_choices)
+    submit = SubmitField('Submit')

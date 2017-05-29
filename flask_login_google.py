d9ee15b28ece55e910087f06acba020615f7fc8f
Marc Rubin <marcrubin6@gmail.com>
2017-05-29 16:56:39 -0700

    updated flask_login_google.py to work with SQLAlchemy

--- flask_login_google.py
+++ flask_login_google.py
@@ -3,6 +3,7 @@ import os
 from flask import Flask, jsonify, redirect, url_for
 from flask_oauth2_login import GoogleLogin
 from flask_login import UserMixin, LoginManager, login_required, login_user, current_user, logout_user
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config.update(
@@ -10,44 +11,68 @@ app.config.update(
     GOOGLE_LOGIN_REDIRECT_SCHEME="http",
 )
 
-app.config['GOOGLE_LOGIN_CLIENT_ID']= ''
-app.config['GOOGLE_LOGIN_CLIENT_SECRET'] = ''
-
+app.config['GOOGLE_LOGIN_CLIENT_ID']= '256102919201-qvjgcstktj5bimq3ffj8k5va85g0r137.apps.googleusercontent.com'
+app.config['GOOGLE_LOGIN_CLIENT_SECRET'] = 'X0HqD1zykF3i4Hvde2QsvkdL'
 google_login = GoogleLogin(app)
+
 login_manager = LoginManager()
 login_manager.init_app(app)
 
-class User(UserMixin):
-    # proxy for a database of users
-    user_database = {"JohnDoe": ("JohnDoe", "John"),
-               "JaneDoe": ("JaneDoe", "Jane")}
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test_user.sqlite'
+app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
 
-    def __init__(self, username, password):
-        self.id = username
-        self.password = password
+class User(db.Model, UserMixin):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    username = db.Column(db.String(20), unique=True)
+    email = db.Column(db.String)
+    name = db.Column(db.String)
+    picture = db.Column(db.String)
 
-    @classmethod
-    def get(cls,id):
-        return cls.user_database.get(id)
+    def __init__(self, username, email, name, picture):
+        self.username = username
+        self.email = email
+        self.name = name
+        self.picture = picture
+
+# class User(UserMixin):
+#     # proxy for a database of users
+#     user_database = {"JohnDoe": ("JohnDoe", "John"),
+#                "JaneDoe": ("JaneDoe", "Jane")}
+#
+#     def __init__(self, username, password):
+#         self.id = username
+#         self.password = password
+#
+#     @classmethod
+#     def get(cls,id):
+#         return cls.user_database.get(id)
+
 
 @login_manager.user_loader
 def load_user(id):
-    print 'load_user ', id
-    user = User.get('plark')
+    print 'requesting user with id =',id
+    user = User.query.filter_by(id=id).first()
     if user is not None:
-        return User(user[0], user[1])
-    return None
+        return user
+    else:
+        return None
+    # print 'load_user ', id
+    # user = User.get('plark')
+    # if user is not None:
+    #     return User(user[0], user[1])
+    # return None
 
 @app.route("/")
 def index():
-    return """
-<html>
-<a href="{}">Login with Google</a>
-""".format(google_login.authorization_url())
+    return '''
+        <a href="%s">Login</a><br>
+    ''' % (url_for('login'))
 
 @app.route("/login")
 def login():
-    print google_login.authorization_url()
     return redirect(google_login.authorization_url())
 
 @app.route('/logout')
@@ -58,15 +83,28 @@ def logout():
 @app.route('/private')
 @login_required
 def private_url():
-    return 'hello'
+    return '''
+        <a href="%s">Logout</a><br>
+        hello: %s<br>
+        <img height="100" src="%s"/>
+    ''' % (url_for('logout'), current_user.name, current_user.picture)
 
 @google_login.login_success
 def login_success(token, profile):
-    print profile['family_name'], profile['email'], profile['hd']
-    user = User(username='plark', password='cat')
-    User.user_database[user.id] = (user.id, user.password)
+    username = profile['email'].split('@')[0]
+
+    #check to see if user in db
+    user = User.query.filter_by(username=username).first()
+    if user is None: #if not, create new user
+        email = profile['email']
+        name = profile['name']
+        picture = profile['picture']
+        user = User(username, email, name, picture)
+        db.session.add(user)
+        db.session.commit()
+
     login_user(user)
-    return jsonify(token=token, profile=profile)
+    return 'LOGIN OK'
 
 @google_login.login_failure
 def login_failure(e):
@@ -77,6 +115,4 @@ if __name__ == "__main__":
 
 
 #TODO:
-# create User database
-# store profile info from google
 # integrate with app

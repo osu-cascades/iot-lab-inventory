a0718b3b5677de91f4c5291d5ba1a2cb8c37a973
Yong Bakos <ybakos@humanoriented.com>
2017-05-31 21:54:41 -0700

    Use flask-appropriate path for database file. Set the database file path to the root directory, since that's where flask db migration commands expect the database file to be.

--- config.py
+++ config.py
@@ -7,4 +7,4 @@ GOOGLE_LOGIN_CLIENT_SECRET = os.environ['GOOGLE_LOGIN_CLIENT_SECRET']
 GOOGLE_LOGIN_REDIRECT_SCHEME = 'http'
 SQLALCHEMY_COMMIT_ON_TEARDOWN = True
 SQLALCHEMY_TRACK_MODIFICATIONS = False
-SQLALCHEMY_DATABASE_URI = 'sqlite:///data.sqlite'
+SQLALCHEMY_DATABASE_URI = "sqlite:///{0}/data.sqlite".format(os.path.join(os.path.abspath(os.path.dirname(__file__))))

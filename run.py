16cd4d1e421b7a55691b05f29065066f51013ca1
Yong Bakos <ybakos@humanoriented.com>
2017-06-07 08:44:35 -0700

    Remove run.py. Fixes #19.

--- run.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python
-# In development, it is preferred to run the app via the `flask` command:
-# FLASK_APP=iot_lab_inventory python -m flask run
-
-from iot_lab_inventory import app
-
-app.run(host='0.0.0.0', port=5000, debug=True)

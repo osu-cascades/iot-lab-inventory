df21db6c2ef11aabade77b14bc7d28629d8d8dd6
Marc Rubin <marcrubin6@gmail.com>
2017-07-07 18:03:49 -0700

    changed Cart from python object to Order database entry (w/ status=Cart)

--- iot_lab_inventory/views/users.py
+++ iot_lab_inventory/views/users.py
@@ -2,7 +2,7 @@ from flask import Blueprint, url_for, redirect, request, render_template, flash,
 from flask_login import login_required, current_user
 from flask_mail import Message
 from iot_lab_inventory import db, mail
-from iot_lab_inventory.models import Part, CartItem, Order
+from iot_lab_inventory.models import Part, Order, OrderItem
 
 users = Blueprint('users', __name__)
 
@@ -19,26 +19,48 @@ def home():
 @users.route('/cart', methods=['GET'])
 @login_required
 def cart():
-  return render_template('cart.html')
+  cart = Order.query.filter_by(user=current_user, status='Cart').first()
+
+  if cart is None:
+    print 'creating new cart'
+    cart = Order()
+  return render_template('cart.html', cart=cart)
 
 
 @users.route('/cart', methods=['POST'])
 @login_required
 def add_part_to_cart():
   part_id = int(request.form['part_id'])
-  if part_id in current_user.cart.cart_items:
-    current_user.cart.cart_items[part_id].quantity += 1
-  else:
-    part = Part.query.filter_by(id=part_id).first()
-    cart_item = CartItem(part.inventory_item, 1)
-    current_user.cart.add(part_id, cart_item)
-  return redirect(url_for('users.cart'))
+  part = Part.query.filter_by(id=part_id).first()
 
+  #get the Order with cart status
+  cart = Order.query.filter_by(user=current_user, status='Cart').first()
+
+  #if Order does not exist, create new Order
+  if cart is None:
+    print 'new cart'
+    cart = Order()
+    cart.status = 'Cart'
+    cart.user = current_user
+
+  # if order_item for part not in Order, create
+  order_item = OrderItem.query.filter_by(order=cart, part=part).first()
+  if order_item is None:
+    order_item = OrderItem(part=part, order=cart, quantity=1)
+    db.session.add(order_item)
+    db.session.commit()
+  # else, add 1 to order_item quantity
+  else:
+    order_item.quantity += 1
+
+  return redirect(url_for('users.cart'))
 
 @users.route('/cart/<int:id>/delete', methods=['POST'])
 @login_required
 def remove_part_from_cart(id):
-  current_user.cart.cart_items.pop(id)
+  order_item = OrderItem.query.filter_by(id=id).first()
+  db.session.delete(order_item)
+  db.session.commit()
   return redirect(url_for('users.cart'))
 
 
@@ -46,7 +68,8 @@ def remove_part_from_cart(id):
 @login_required
 def update_part_in_cart(id):
   quantity = request.form.get('quantity')
-  current_user.cart.cart_items[id].quantity = int(quantity)
+  order_item = OrderItem.query.filter_by(id=id).first()
+  order_item.quantity = int(quantity)
   return redirect(url_for('users.cart'))
 
 
@@ -64,25 +87,22 @@ def order(id):
 @users.route('/orders', methods=['POST'])
 @login_required
 def create_order():
+  cart = Order.query.filter_by(user=current_user, status='Cart').first()
+
   #check for empty cart
-  if not current_user.cart.cart_items:
+  if cart is None:
     flash('No items in cart.')
     return redirect(url_for('users.cart'))
 
   #check inventory for quantity
-  for part_id in current_user.cart.cart_items:
-    part = Part.query.filter_by(id=part_id).first()
-    quantity = part.inventory_item.quantity
-    cart_item = current_user.cart.cart_items[part_id]
-    if cart_item.quantity > quantity:
-      msg = 'Unable to create order: not enough ' + part.name + ' in inventory.'
+  for order_item in cart.order_items:
+    part = order_item.part
+    if part.inventory_item.quantity < order_item.quantity:
+      msg = 'Unable to create order: too few ' + part.name + ' in inventory.'
       flash(msg)
       return redirect(url_for('users.cart'))
 
-  order = Order(current_user.cart)
-  db.session.add(order)
-  db.session.commit()
-  current_user.cart.cart_items.clear()
+  cart.status = 'Pending'
 
   # #send email to user saying order is pending..
   # msg = Message(subject='OSU-Cascades IoT-Lab: Order Pending ', \
@@ -91,4 +111,4 @@ def create_order():
   # msg.html = render_template('mail/pending.html', order=order)
   # mail.send(msg)
 
-  return redirect(url_for('users.order', id=order.id))
+  return redirect(url_for('users.order', id=cart.id))
